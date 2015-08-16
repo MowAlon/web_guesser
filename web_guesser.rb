62b240b7c2cd70067f00db40e4535efc04b094ac
Alon Waisman <mowalon@yahoo.com>
2015-08-16 17:02:37 -0600

    I4: extension complete, colored backgrounds

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,15 +2,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :number, rand(101)
+set :status, :way_wrong
 
 def check_guess(guess)
   guess = guess.to_i
   message = if guess == settings.number
+    settings.status = :correct
      "Whoa, dude... good guess. You got it right!"
-  elsif guess > settings.number
-    guess - settings.number > 5 ? "Way too high!" : "Too high!"
+  elsif guess.between?(settings.number - 5, settings.number + 5)
+    settings.status = :close
+    guess < settings.number ? "Too low!" : "Too high!"
   else
-    settings.number - guess > 5 ? "Way too low!" : "Too low!"
+    guess < settings.number ? "Way too low!" : "Way too high!"
   end
 end
 
@@ -18,5 +21,5 @@ get '/' do
   guess = params['guess']
   message = check_guess(guess)
 
-  erb :index, :locals => {:number => settings.number, :message => message}
+  erb :index, :locals => {:number => settings.number, :message => message, :status => settings.status}
 end
